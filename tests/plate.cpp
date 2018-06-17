c2efb8f7489e4c3969bcfa0e0061a2cf0d55fd97
Nathan J. Smith <nathanjsmith@gmail.com>
2018-06-17 09:07:46 -0500

    Converted the plate unit test to use doctest

--- tests/plate.cpp
+++ tests/plate.cpp
@@ -30,9 +30,12 @@
 
 #include <calico/utilities/meshes/plate.hpp>
 
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
 #include <iostream>
 
-int main(int argc, const char *argv[]) {
+TEST_CASE("fire a single ray against a flat 1x1 plate") {
 
   typedef double Float;
   typedef calico::utilities::meshes::Plate<Float> Mesh;
@@ -48,10 +51,10 @@ int main(int argc, const char *argv[]) {
   Mesh::FaceId start_face_id[1] = {Mesh::ray_miss_id_c};
   Float start_x[1]     = {0.};
   Float start_y[1]     = {0.};
-  Float start_z[1]     = {10.};
+  Float start_z[1]     = {10.}; // start above the plate
   Float direction_x[1] = {0.};
   Float direction_y[1] = {0.};
-  Float direction_z[1] = {-1.};
+  Float direction_z[1] = {-1.}; // fire down towards the plate
   auto rays = 
     calico::input::make_soa_input<Float, Mesh::FaceId>
             (1, start_face_id, 
@@ -64,18 +67,24 @@ int main(int argc, const char *argv[]) {
   Float hit_x[1]                 = {-1000.};
   Float hit_y[1]                 = {-1000.};
   Float hit_z[1]                 = {-1000.};
+
+  // Create a SoaResults object that adapts (provides a view into) the
+  // strike_face_id, t, hit_x, hit_y, and hit_z arrays.
   auto results = 
     calico::result::make_soa_result<Float, std::size_t>(strike_face_id, t, hit_x, hit_y, hit_z);
 
 
+  // Trace our 1 ray against the plate and record the results through the
+  // "results" adapter into the strike_face_id, t, and hit_* arrays.
   tracer.trace_rays(rays, results);
 
-
-  std::cerr << "Hit facet " << strike_face_id[0] << " at (" 
-            << hit_x[0] << ", " << hit_y[0] << ", " << hit_z[0] 
-            << ")" << std::endl;
-
-  return 0;
+  // Verify the ray struck the surface we expected in the location we expected.
+  // It's actually ambiguous whether the ray will strike facet 0 or 1. The ray
+  // should strike along the edge shared by the two facets.
+  REQUIRE(bool(strike_face_id[0] == 0u || strike_face_id[0] == 1u));
+  REQUIRE(hit_x[0] == doctest::Approx(0.));
+  REQUIRE(hit_y[0] == doctest::Approx(0.));
+  REQUIRE(hit_z[0] == doctest::Approx(3.));
 }
 //=============================================================================
 
